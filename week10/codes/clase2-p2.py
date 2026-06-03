6b161865b3b030483106369f1e8043af8bd8edb3
Joaquin Peralta <jperaltac@gmail.com>
2026-06-03 10:10:07 -0400

    Align week10 class 2 scripts with notebook

--- week10/codes/clase2-p2.py
+++ week10/codes/clase2-p2.py
@@ -1,26 +1,25 @@
-import tensorflow as tf
-
 tf.keras.utils.set_random_seed(123)
 
-clf = tf.keras.Sequential([
-    tf.keras.layers.Input(shape=(X_train_s.shape[1],)),
+transit_model = tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(tr_X_train_s.shape[1],)),
     tf.keras.layers.Dense(64, activation="relu"),
     tf.keras.layers.Dense(32, activation="relu"),
     tf.keras.layers.Dense(1, activation="sigmoid"),
 ])
 
-clf.compile(
+transit_model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
     loss="binary_crossentropy",
     metrics=["accuracy", tf.keras.metrics.AUC(name="auc")],
 )
 
-history = clf.fit(
-    X_train_s, y_train,
-    validation_data=(X_val_s, y_val),
+transit_history = transit_model.fit(
+    tr_X_train_s, tr_y_train,
+    validation_data=(tr_X_val_s, tr_y_val),
     epochs=25,
     batch_size=256,
     verbose=0,
 )
 
-print(dict(zip(clf.metrics_names, clf.evaluate(X_test_s, y_test, verbose=0))))
+results = transit_model.evaluate(tr_X_test_s, tr_y_test, verbose=0)
+print(dict(zip(transit_model.metrics_names, results)))

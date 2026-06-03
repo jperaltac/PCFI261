a4567adc7ff64e275e8097b0be169ac8a1c97102
Joaquin Peralta <jperaltac@gmail.com>
2026-06-03 10:09:17 -0400

    Align week10 class 1 scripts with notebook

--- week10/codes/clase1-p2.py
+++ week10/codes/clase1-p2.py
@@ -1,28 +1,26 @@
-import tensorflow as tf
-
 tf.keras.utils.set_random_seed(42)
 
-model = tf.keras.Sequential([
+osc_model = tf.keras.Sequential([
     tf.keras.layers.Input(shape=(5,)),
     tf.keras.layers.Dense(64, activation="relu"),
     tf.keras.layers.Dense(64, activation="relu"),
     tf.keras.layers.Dense(1),
 ])
 
-model.compile(
+osc_model.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
     loss="mse",
     metrics=[tf.keras.metrics.RootMeanSquaredError(name="rmse")],
 )
 
-history = model.fit(
-    X_train_s, y_train,
-    validation_data=(X_val_s, y_val),
+osc_history = osc_model.fit(
+    osc_X_train_s, osc_y_train,
+    validation_data=(osc_X_val_s, osc_y_val),
     epochs=40,
     batch_size=256,
     verbose=0,
 )
 
-test_loss, test_rmse = model.evaluate(X_test_s, y_test, verbose=0)
-print(f"MSE test:  {test_loss:.5f}")
-print(f"RMSE test: {test_rmse:.5f}")
+osc_test_loss, osc_test_rmse = osc_model.evaluate(osc_X_test_s, osc_y_test, verbose=0)
+print(f"MSE test:  {osc_test_loss:.5f}")
+print(f"RMSE test: {osc_test_rmse:.5f}")
